e59e246ba1cea6f5b02e4c25d6bb563a5b8c9471
ananydhar <ananyadhar@somaiya.edu>
2023-01-28 23:32:33 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+import { AuthContextProvider } from './context/auth-context';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//using AuthContext Component in the root file
+root.render(
+<AuthContextProvider>
+    <App />
+</AuthContextProvider>
+);
